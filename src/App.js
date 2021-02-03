3bcacd1944a119f2f2415fc4a35bd35fe0d24196
jito <jitokj3@gmail.com>
2021-02-03 23:58:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Hidden from "@material-ui/core/Hidden";
+
+import Drawer from "./components/Drawer/Drawer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Hidden smUp>
+          <Grid item xs={12}>
+            {/* <Paper className={classes.paper}>drawer</Paper> */}
+            <Drawer />
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>1</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>2</Paper>
+          </Grid>
+        </Hidden>
+        <Hidden smDown>
+          <Grid item md={2}>
+            <Paper className={classes.paper}>drawer</Paper>
+          </Grid>
+          <Grid item md={10}>
+            <Paper className={classes.paper}>1</Paper>
+          </Grid>
+        </Hidden>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import PersonIcon from "@material-ui/icons/Person";
+import ContactsIcon from "@material-ui/icons/Contacts";
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#1f1f1f",
+  },
+  divider: {
+    backgroundColor: "#ffffff",
+    marginRight: "20px",
+  },
+  paper: {
+    background: "#1f1f1f",
+  },
+
+  heading: {
+    color: "#ffffff",
+  },
+
+  listItem: {
+    color: "#ffffff",
+  },
+
+  list: {
+    width: 200,
+    backgroundColor: "#1f1f1f",
+    marginLeft: "20px",
+    marginTop: "10px",
+  },
+});
+
+const Drawer = () => {
+  const styles = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const sideDrawer = (anchor) => (
+    <div
+      className={styles.list}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Typography classes={{ h6: styles.heading }} variant="h6">
+        Puzzle
+      </Typography>
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <CameraAltIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"Media Library"}
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <PersonIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"About Me"}
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <ContactsIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"Contacts"}
+          />
+        </ListItem>
+      </List>
+      <Divider classes={{ root: styles.divider }} />
+
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <CameraAltIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"Media Library"}
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <PersonIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"About Me"}
+          />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <ContactsIcon style={{ color: "#ffffff" }} />
+          </ListItemIcon>
+          <ListItemText
+            classes={{ primary: styles.listItem }}
+            primary={"Contacts"}
+          />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={styles.root}>
+      <React.Fragment>
+        <IconButton onClick={toggleDrawer("left", true)}>
+          <MenuIcon style={{ color: "#ffffff" }} />
+        </IconButton>
+        <SwipeableDrawer
+          classes={{ paper: styles.paper }}
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+          onOpen={toggleDrawer("left", true)}
+        >
+          {sideDrawer("left")}
+        </SwipeableDrawer>
+      </React.Fragment>
+    </div>
+  );
+};
+
+export default Drawer;
